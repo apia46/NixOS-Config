e5788df0d1a144f06761400a3c6b8442a18a5568
Apia <96461146+apia46@users.noreply.github.com>
2026-06-15 11:50:41 +1200

    thats not very deterministic of you

--- andan/home.nix
+++ andan/home.nix
@@ -62,6 +62,13 @@
         geequlim.godot-tools
         ms-dotnettools.csharp
         ms-dotnettools.vscode-dotnet-runtime
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "kdl";
+            publisher = "kdl-org";
+            version = "2.1.3";
+            sha256 = "sha256-Jssmb5owrgNWlmLFSKCgqMJKp3sPpOrlEUBwzZSSpbM";
+          }
         ];
     };
   };
