6e5b5c4a5a948a02bfb68c4e22cc9893409cd8b0
Apia <96461146+apia46@users.noreply.github.com>
2026-06-15 23:08:26 +1200

    please

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs";
     home-manager.url = "github:nix-community/home-manager?ref=release-26.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=v0.7.0";
     wifi-manager.url = "github:Vijay-papanaboina/wifi-manager";
