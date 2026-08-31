5104784a4e1dd40b0c08845d8abb28c62c571095
Apia <96461146+apia46@users.noreply.github.com>
2026-08-31 19:50:26 +1200

    murale2

--- derivations/murale.nix
+++ derivations/murale.nix
@@ -29,7 +29,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     mpv
   ];
 
-  cargoHash = "sha256-+jlkV+umcbHpPYbwdx0qrzuMkxA7RkSQ2BoYy0xEkck=";
+  cargoHash = "sha256-i5DMAUfgDnrQSPNwTwToseNHu0iFHObvaZ6Fn++fTS4=";
 
   meta = {
     description = "Lean, memory-safe video wallpaper player for Wayland compositors";
